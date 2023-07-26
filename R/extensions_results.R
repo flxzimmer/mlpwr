628c5e1b912c464caf5eea24ad673ee4c68ee2bc
Felix <49242603+flxzimmer@users.noreply.github.com>
2023-07-27 00:52:08 +0200

    added data for extensions vignette so it runs quickly

---
+++ R/extensions_results.R
@@ -0,0 +1,6 @@
+#' Results of the extensions vignette
+#'
+#' Since the Monte Carlo simulations required to generate the results in the extensions vignette can be time-consuming, we have precomputed them and included them as a data file.
+#'
+#' @format A list with designresult objects created in the vignette "extensions"
+"extensions_results"
